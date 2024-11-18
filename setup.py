8356ceb437087626e6b9bdb5c321eb1dc9d1d0bb
Ilma Aliya Fiddien <52804524+fiddien@users.noreply.github.com>
2024-11-18 20:52:32 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A package to convert numbers to Indonesian words",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Kecilin-Team/indonesian-number-normalizer",
+    url="https://github.com/fiddien/indonesian-number-normalizer",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
